d79cefe8222eaa36c0965538d7bf0b9cc3978979
Dante Zulli <dantezulli2004@gmail.com>
2025-05-29 20:29:00 -0300

    chore: Elimina hibernate nativo y comienza la migracion a Spring Hibernate con JPA

--- src/main/java/com/grupo25/tp_obj2_hibernate/TpObj2HibernateApplication.java
+++ src/main/java/com/grupo25/tp_obj2_hibernate/TpObj2HibernateApplication.java
@@ -1,10 +1,7 @@
 package com.grupo25.tp_obj2_hibernate;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TpObj2HibernateApplication {
@@ -13,13 +10,4 @@ public class TpObj2HibernateApplication {
 		SpringApplication.run(TpObj2HibernateApplication.class, args);
 	}
 
-	@Bean
-	public SessionFactory sessionFactory() {
-		try {
-			return new Configuration().configure().buildSessionFactory();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
 }
